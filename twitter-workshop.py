9994b1bf1983eecccf2a2e8727070cee4a81c08a
H-4-N-5 <hansheidmann@gmail.com>
2015-09-16 17:12:42 -0600

    added more tweets to txt

--- twitter-workshop.py
+++ twitter-workshop.py
@@ -3,7 +3,7 @@ from twython import Twython, TwythonError
 # Requires Authentication as of Twitter API v1.1
 twitter = Twython('ffTvOIZqqWn9japC6mJ8n02Mc', 'L9lYgDMUfXi2Ev6iVSmukGOJlkdMobOR0UToFNDKlai81RnE5W', '499163854-N4BesXw2V5wFwH2vnTChqwtSmdRP2MY9JqXXXhUB', 'oNNox40GznN1hMY1TDGJLTojUObY9B30993uAogUFWd6c')
 try:
-    search_results = twitter.search(q='Global Warming', count=50)
+    search_results = twitter.search(q='Climate Change', count=50)
 except TwythonError as e:
     print e
 
